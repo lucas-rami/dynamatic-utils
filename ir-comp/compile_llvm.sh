7dceaed5c37285783b2f11a769a1abca45567911
Lucas Ramirez <lucas.ramirez@epfl.ch>
2022-09-15 21:16:16 +0200

    Cleaner output for LLVM compile flow and env. var.

--- ir-comp/compile_llvm.sh
+++ ir-comp/compile_llvm.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
-
 # === Usage ===
 # Arguments are name of the benchmark to compile into LLVM IR
 
 # Remember where we started from
 SCRIPT_DIR=$PWD
 
-# Define some variables
-DYNAMATIC_PATH=/home/ramirez/dynamatic
-DYNAMATIC_TOOLS_PATH=/home/ramirez/dynamatic/tools
-
 # Source environment variables for Dynamatic and go to folder containing benchmarks
-cd $DYNAMATIC_PATH
+cd "$DYNAMATIC_PATH"
 source .env
 cd dhls/etc/dynamatic/elastic-circuits/examples
 
-for bench_name in "$@" 
+idx=1
+N_BENCH=$(($#-1))
+for bench_name in "$@"; 
 do
-    # Compile becnhmark
-    echo "Compiling benchmark ${bench_name}..."
-    make name=${bench_name} graph > /dev/null 2>&1
-
-    # Check if make succeeded
-    if [ $? -eq 0 ]; 
-    then 
-        # Move the build files from the [...]/examples/_build folder to the folder where this script was ran from
-        echo "Successful. Copying results for benchmark ${bench_name}"
-        LLVM_FOLDER="${SCRIPT_DIR}/benchmarks/${bench_name}/llvm"
-        mkdir -p "${LLVM_FOLDER}"
-        cp "_build/${bench_name}/"*.ll "${LLVM_FOLDER}/"
-    else 
-        echo "Failed to compile benchmark ${bench_name}" 
+    LLVM_FOLDER="$SCRIPT_DIR/benchmarks/$bench_name/llvm"
+    echo "[$idx/$N_BENCH] Compiling benchmark $bench_name..."
+    idx=$((idx+1))
+    
+    # Stop if benchmark is already compiled
+    if [ -d "$LLVM_FOLDER" ]; then
+        echo "Already compiled"
+        continue
     fi
+
+    # Compile benchmark
+    make name=$bench_name graph > /dev/null 2>&1
+
+    # Stop if make fails
+    if [ $? -ne 0 ]; then 
+        echo "Failed"
+        continue
+    fi 
+
+    # Move the build files from the [...]/examples/_build folder to the folder
+    # where this script was ran from
+    echo "Successful"
+    mkdir -p "$LLVM_FOLDER"
+    cp "_build/$bench_name/"*.ll "$LLVM_FOLDER/"
 done
+
+echo "Done!"
