d922ef2617e921ad7be7dba1550e6cbfcf2ca395
Lucas Ramirez <lucas.rami@proton.me>
2023-05-08 14:54:13 +0200

    [FPL22] Added missing FPL22 benchmarks
    This adds the fir and binary_search benchmarks to the FPL22 folder.

---
+++ benchmarks/FPL22/binary_search/src/binary_search.cpp
@@ -0,0 +1,52 @@
+#include "binary_search.h"
+//------------------------------------------------------------------------
+// binary_search
+//------------------------------------------------------------------------
+
+// SEPARATOR_FOR_MAIN
+
+#include <stdlib.h>
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#define AMOUNT_OF_TEST 1
+
+int binary_search(in_int_t in0, in_int_t mem0[N]) {
+  int idx_1 = -1;
+  int idx_2 = -1;
+
+  for (int i = 0; i < N; i += 2) {
+    if (mem0[i] == in0) {
+      idx_1 = i;
+      break;
+    }
+  }
+
+  for (int i = 1; i < N; i += 2) {
+    if (mem0[i] == in0) {
+      idx_2 = i;
+      break;
+    }
+  }
+
+  int done = -1;
+  if (idx_1 != -1)
+    done = idx_1;
+  else if (idx_2 != -1)
+    done = idx_2;
+
+  return done;
+}
+
+int main(void) {
+  in_int_t mem0[N];
+
+  for (int i = 0; i < N; i++) {
+    mem0[i] = i;
+  }
+
+  binary_search(55, mem0);
+}
---
+++ benchmarks/FPL22/fir/src/fir.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define AMOUNT_OF_TEST 1
+
+typedef int in_int_t;
+typedef int inout_int_t;
+
+int fir(in_int_t mem0[1000], in_int_t mem1[1000]);
